3ab0d9c8dcea7496d540e179352dc5812d240e96
bkpathk <bkpathk@mtu.edu>
2014-12-05 18:40:03 -0800

    custom input format

---
+++ src/main/java/com/cloudwick/mapreduce/customlogoutputformat/LogRecordWriter.java
@@ -0,0 +1,34 @@
+package com.cloudwick.mapreduce.customlogoutputformat;
+
+
+import org.apache.hadoop.mapreduce.RecordWriter;
+import org.apache.hadoop.mapreduce.TaskAttemptContext;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+
+/**
+ * Created by bijay on 12/5/14.
+ */
+public class LogRecordWriter<K, V> extends RecordWriter<K, V> {
+
+    private DataOutputStream out;
+
+    public LogRecordWriter(DataOutputStream out) {
+        this.out = out;
+    }
+
+    @Override
+    public void close(TaskAttemptContext context) throws IOException {
+        out.close();
+    }
+
+    @Override
+    public void write(K key, V value) throws IOException {
+        // format output by separating the key value with =>
+        // and surround the value with ()
+
+        String outString = key.toString() + " => " + "( " + value.toString() + ")";
+        out.writeBytes(outString);
+    }
+}
