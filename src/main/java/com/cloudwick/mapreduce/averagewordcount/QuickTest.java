945f611c17e871dd40fb774f1dd96330891d66b5
bkpathk <bkpathk@mtu.edu>
2014-12-08 14:55:46 -0800

    some update and added mrtest for wordcount

---
+++ src/main/java/com/cloudwick/mapreduce/averagewordcount/QuickTest.java
@@ -0,0 +1,25 @@
+package com.cloudwick.mapreduce.averagewordcount;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Created by bijay on 12/8/14.
+ */
+public class QuickTest {
+
+    public static void main(String[] args) {
+        Map<String, Long> keyMap = new HashMap<String, Long>();
+
+        keyMap.put("abc", 5l);
+        keyMap.put("bcd", 10l);
+        keyMap.put("asd", 23l);
+        Set<String> keys = keyMap.keySet();
+
+        for (String k : keys) {
+            System.out.println(k + "   " + keyMap.get(k));
+        }
+
+    }
+}
---
+++ src/test/java/com/cloudwick/mapreduce/averagewordcount/AveragePerMapperTesting.java
@@ -0,0 +1,60 @@
+package com.cloudwick.mapreduce.averagewordcount;
+
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mrunit.mapreduce.MapDriver;
+import org.apache.hadoop.mrunit.mapreduce.MapReduceDriver;
+import org.apache.hadoop.mrunit.mapreduce.ReduceDriver;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.nio.DoubleBuffer;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by bijay on 12/8/14.
+ */
+public class AveragePerMapperTesting {
+
+    MapDriver<LongWritable, Text, Text, IntWritable> mapDriver;
+
+    ReduceDriver<Text, IntWritable, Text, IntWritable> combinerDriver;
+    MapReduceDriver<LongWritable, Text, Text, IntWritable, Text, DoubleWritable> mapReduceDriver;
+
+    @Before
+
+    public void setUp() {
+        TokenizerMapper mapper = new TokenizerMapper();
+        AverageReducer reducer = new AverageReducer();
+        MapperAverageCombiner combiner = new MapperAverageCombiner();
+        mapDriver = MapDriver.newMapDriver(mapper);
+        combinerDriver = ReduceDriver.newReduceDriver(combiner);
+        mapReduceDriver = MapReduceDriver.newMapReduceDriver(mapper, reducer);
+
+    }
+
+    @Test
+    public void testMapper() throws IOException {
+        mapDriver.withInput(new LongWritable(), new Text("abc\tbcd"))
+                .withOutput(new Text("abc"), new IntWritable(1))
+                .withOutput(new Text("bcd"), new IntWritable(1))
+                .runTest();
+    }
+
+    @Test
+    public void testCombiner() throws IOException {
+        List<IntWritable> values = new ArrayList<IntWritable>();
+        values.add(new IntWritable(1));
+        values.add(new IntWritable(1));
+
+        combinerDriver.withInput(new Text("abc"), values)
+                .withOutput(new Text("abc"), new IntWritable(2))
+                .runTest();
+    }
+}
