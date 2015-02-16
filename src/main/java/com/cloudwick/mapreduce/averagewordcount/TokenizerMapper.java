1656876fae14ffc2e3c05a5649c0a9b6079d4759
bkpathak <bkpathak@mtu.edu>
2015-02-16 13:08:34 -0800

    added target to ignore

--- src/main/java/com/cloudwick/mapreduce/averagewordcount/TokenizerMapper.java
+++ src/main/java/com/cloudwick/mapreduce/averagewordcount/TokenizerMapper.java
@@ -13,17 +13,17 @@ import java.util.StringTokenizer;
  * Created by bijay on 11/26/14.
  */
 public class TokenizerMapper extends Mapper<LongWritable, Text, Text, IntWritable> {
-    private IntWritable one = new IntWritable(1);
-    private Text word = new Text();
+  private IntWritable one = new IntWritable(1);
+  private Text word = new Text();
 
 
-    @Override
-    public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
-        StringTokenizer itr = new StringTokenizer(value.toString());
-        while (itr.hasMoreTokens()) {
-            word.set(itr.nextToken());
-            context.write(word, one);
-        }
+  @Override
+  public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+    StringTokenizer itr = new StringTokenizer(value.toString());
+    while (itr.hasMoreTokens()) {
+      word.set(itr.nextToken());
+      context.write(word, one);
     }
+  }
 }
 
