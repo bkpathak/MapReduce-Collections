fb94fa73ebc073317edc94ccf529e5bad0e001a0
bkpathak <bkpathak@mtu.edu>
2015-02-16 14:17:10 -0800

    formated with two space indent

--- src/main/java/com/cloudwick/mapreduce/custominputformat/FixedWidthColumnDriver.java
+++ src/main/java/com/cloudwick/mapreduce/custominputformat/FixedWidthColumnDriver.java
@@ -18,43 +18,43 @@ import java.io.IOException;
  */
 public class FixedWidthColumnDriver extends Configured implements Tool {
 
-    @Override
-    public int run(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
-        if (args.length != 2) {
-            System.out.printf("Usage: " + this.getClass().getName() +
-                    "<in dir> <out dir> \n");
-            System.exit(-1);
-        }
-
-        Configuration conf = getConf();
-        /**
-         * Provide the information for the each record, i.e,
-         * The number of field in the record including
-         */
-        FixedWidthColumnInputFormat.setRecordLength(conf, 50);
-        FixedWidthColumnRecordReader.setNumValue(conf, 4);
-        FixedWidthColumnRecordReader.setValuesWidth(conf, "7", "25", "10", "8");
-
-
-        Job job = Job.getInstance(conf);
-
-        job.setJobName("Fixed column width format");
-        job.setJarByClass(FixedWidthColumnDriver.class);
-
-        FileInputFormat.setInputPaths(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
-
-        job.setInputFormatClass(FixedWidthColumnInputFormat.class);
-        job.setNumReduceTasks(0);
-
-
-        int ret = job.waitForCompletion(true) ? 0 : 1;
-        return ret;
-
+  @Override
+  public int run(String[] args) throws IOException, ClassNotFoundException, InterruptedException {
+    if (args.length != 2) {
+      System.out.printf("Usage: " + this.getClass().getName() +
+              "<in dir> <out dir> \n");
+      System.exit(-1);
     }
 
-    public static void main(String[] args) throws Exception {
-        int res = ToolRunner.run(new Configuration(), new FixedWidthColumnDriver(), args);
-        System.exit(res);
-    }
+    Configuration conf = getConf();
+    /**
+     * Provide the information for the each record, i.e,
+     * The number of field in the record including
+     */
+    FixedWidthColumnInputFormat.setRecordLength(conf, 50);
+    FixedWidthColumnRecordReader.setNumValue(conf, 4);
+    FixedWidthColumnRecordReader.setValuesWidth(conf, "7", "25", "10", "8");
+
+
+    Job job = Job.getInstance(conf);
+
+    job.setJobName("Fixed column width format");
+    job.setJarByClass(FixedWidthColumnDriver.class);
+
+    FileInputFormat.setInputPaths(job, new Path(args[0]));
+    FileOutputFormat.setOutputPath(job, new Path(args[1]));
+
+    job.setInputFormatClass(FixedWidthColumnInputFormat.class);
+    job.setNumReduceTasks(0);
+
+
+    int ret = job.waitForCompletion(true) ? 0 : 1;
+    return ret;
+
+  }
+
+  public static void main(String[] args) throws Exception {
+    int res = ToolRunner.run(new Configuration(), new FixedWidthColumnDriver(), args);
+    System.exit(res);
+  }
 }
